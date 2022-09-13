5075337a33c616859142c5fe08ab874afaa053ea
marufmarzuq <marufmarzuq18@gmail.com>
2022-09-14 01:32:11 +0600

    Confi edited

---
+++ config.js
@@ -0,0 +1,19 @@
+secret_key = 1234asdf
+
+PORT=8080
+
+################## CLOUD RUN ##############
+CLOUD_RUN_ORDERS_SERVICE_NAME=oms-react-client-prod
+
+################## Dev DB ###################
+DB_USER=postgres
+DB_HOST=35.202.220.114
+DB_DATABASE=postgres
+DB_PASSWORD=aqai@1b@2025
+DB_PORT=5432
+
+################## JWT AUTH ##################
+JWT_SECRET=AQAI2025
+
+################ SERVER URL ##################
+REACT_APP_API_URL=https://oms-node-server-prod-5olodvvprq-uc.a.run.app
